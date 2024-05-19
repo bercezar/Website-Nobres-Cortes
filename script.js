d6e6cc1ef3410f22e18c62ca8c5b0315debaa595
Bernardo Cezar <b.cezar1812@gmail.com>
2024-05-19 00:50:11 -0300

    fix: alteração no setInterval e no nextImage() // Personalização na área do cadastro, mudança no background-color da body, alterações css no forms

--- script.js
+++ script.js
@@ -1,30 +1,13 @@
 let count = 1;
-let interval;
-
 document.getElementById("radio1").checked = true;
 
-function startInterval() {
-  interval = setInterval(function () {
-    nextImage();
-  }, 4000);
-}
-
-function resetInterval() {
-  clearInterval(interval);
-  startInterval();
-}
+setInterval(function () {
+  nextImage();
+}, 3500);
 
 function nextImage() {
   count++;
-  if (count > 5) count = 1;
+  if (count > 4) count = 1;
+
   document.getElementById("radio" + count).checked = true;
 }
-
-document.querySelectorAll('input[name="radio-button"]').forEach((radio) => {
-  radio.addEventListener("change", function () {
-    count = parseInt(this.id.replace("radio", ""));
-    resetInterval();
-  });
-});
-
-startInterval();
