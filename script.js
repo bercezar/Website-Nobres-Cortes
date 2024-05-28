f9e37f5aa9ec643e2051edf5154e522970ec9f9c
Bernardo Cezar <b.cezar1812@gmail.com>
2024-05-28 16:45:04 -0300

    remoção do 'quem somos' do navBar, mudança de estilo no 'middle informs', adição de um container 'imgtext times', aplicação de um accordion com estilo e uso do J.S, atualização da estilização do  formulário

--- script.js
+++ script.js
@@ -11,3 +11,18 @@ function nextImage() {
 
   document.getElementById("radio" + count).checked = true;
 }
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
