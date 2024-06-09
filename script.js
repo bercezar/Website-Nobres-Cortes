7d7b0038ca20626459f1a88c265dc52a40ae8e4b
Bernardo Cezar <b.cezar1812@gmail.com>
2024-06-09 02:30:54 -0300

    fix: remoção de bug no JS do accordion

--- script.js
+++ script.js
@@ -20,13 +20,19 @@ function resetInterval() {
   intervalId = setInterval(nextImage, 3500);
 }
 
-var acc = document.getElementsByClassName("accordion");
-var i;
+let acc = document.getElementsByClassName("accordion");
 
-for (i = 0; i < acc.length; i++) {
+for (let i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function () {
+    for (let j = 0; j < acc.length; j++) {
+      if (acc[j] !== this) {
+        acc[j].classList.remove("active");
+        acc[j].nextElementSibling.style.maxHeight = null;
+      }
+    }
+
     this.classList.toggle("active");
-    var panel = this.nextElementSibling;
+    let panel = this.nextElementSibling;
     if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
     } else {
